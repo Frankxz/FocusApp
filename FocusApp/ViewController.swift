9579dceeb1057cb61e1879fe379e73a7161c359d
Miller <gelaseen@gmail.com>
2022-02-28 01:19:54 +0700

    Done welcomeViewController elements

--- FocusApp/ViewController.swift
+++ FocusApp/ViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var welcomeLabel = UILabel()
+    var welcomeButton = UIButton(type: .system)
+    var textLabel = UILabel()
+    var circleView = UIView()
+    var timerButton = UIButton()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setUI()
+        welcomeButton.addTarget(self, action: #selector(welcomeButtonTouched), for: .touchUpInside)
+        
     }
-
-
+    @objc func welcomeButtonTouched(){
+        print("hello")
+    }
+    
+    func setUI(){
+        view.backgroundColor = .white
+        
+        view.addSubview(welcomeLabel)
+        welcomeLabel.translatesAutoresizingMaskIntoConstraints = false
+        welcomeLabel.text = "Start to work with more productivity with us ! We will teach you time management 🤫"
+        welcomeLabel.numberOfLines = 0
+        welcomeLabel.textAlignment = .center
+        
+        welcomeLabel.font = UIFont(name: "AppleSDGothicNeo-SemiBold", size: 25)
+        welcomeLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        welcomeLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        welcomeLabel.widthAnchor.constraint(equalToConstant: view.frame.width / 1.5).isActive = true
+        
+        view.addSubview(welcomeButton)
+        welcomeButton.translatesAutoresizingMaskIntoConstraints = false
+        welcomeButton.setTitle("Start improve your time spending", for: .normal)
+        welcomeButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        welcomeButton.topAnchor.constraint(equalTo: welcomeLabel.bottomAnchor, constant: 40).isActive = true
+        
+    }
+    
 }
 
